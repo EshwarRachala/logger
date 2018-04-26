fec85f4135742908540e1181b3b70a874164028a
eshwar rachala <eshwar.appdev@gmail.com>
2018-04-25 23:10:05 -0400

    initial setup

---
+++ classLib/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace classLib {
+    public static class Logger {
+        public static IWebHostBuilder EnableLogger (this IWebHostBuilder builder) {
+
+            Log.Logger = new LoggerConfiguration ()
+                .MinimumLevel.Debug ()
+                .MinimumLevel.Override ("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext ()
+                .WriteTo.Console ()
+                .CreateLogger ();
+
+            builder.UseSerilog ();
+
+            return builder;
+        }
+
+    }
+}
