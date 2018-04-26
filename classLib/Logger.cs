5c77a41d257e603d39fd186bebad35b2359f9477
eshwar rachala <eshwar.appdev@gmail.com>
2018-04-26 00:24:44 -0400

    updated

--- classLib/Logger.cs
+++ classLib/Logger.cs
@@ -9,18 +9,26 @@ namespace classLib {
 
     public static class Logger {
 
-        public static LoggerConfiguration logger;
-        public static IWebHostBuilder EnableLogger (this IWebHostBuilder builder,
-            IConfiguration configuration = null, IHostingEnvironment environment = null) {
+        public static LoggerConfiguration logger =
+            new LoggerConfiguration ()
+            .MinimumLevel.Debug ()
+            .MinimumLevel.Override ("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext ();
 
-            Log.Logger = logger.CreateLogger ();
-
-            builder.UseSerilog ();
+        public static IWebHostBuilder AddConsole (this IWebHostBuilder builder) {
+            logger.WriteTo.Console (theme: AnsiConsoleTheme.Code,
+                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
 
             return builder;
         }
 
-        public static IWebHostBuilder AddConsole (this IWebHostBuilder builder) {
+        public static IWebHostBuilder AddSqlLite (this IWebHostBuilder builder) {
+
+            logger.WriteTo.SQLite (@"Logs\log.db");
+            return builder;
+        }
+
+        public static IWebHostBuilder AddEmail (this IWebHostBuilder builder) {
 
             logger = new LoggerConfiguration ()
                 .MinimumLevel.Debug ()
@@ -29,6 +37,15 @@ namespace classLib {
 
             logger.WriteTo.Console (theme: AnsiConsoleTheme.Code,
                 outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+
+            return builder;
+        }
+        public static IWebHostBuilder EnableLogger (this IWebHostBuilder builder,
+            IConfiguration configuration = null, IHostingEnvironment environment = null) {
+
+            Log.Logger = logger.CreateLogger ();
+
+            builder.UseSerilog ();
 
             return builder;
         }
